7beeeb86f332ffd3041aa308ec891f4939ae0a54
NirGeller-reichman <nir.gellerchipchev@post.runi.ac.il>
2025-11-22 13:06:56 +0200

    1st

--- Primes.java
+++ Primes.java
@@ -1,5 +1,30 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        String input =args[0];
+        int n = Integer.parseInt(input);
+        boolean arr[] = new boolean[n+1];
+        for (int i=2; i<=n; i++){
+            arr[i]= true;
+        
     }
+    arr[0] = false;
+    arr[1] = false;
+        
+    
+    
+    for (int i=2; i*i<=n; i++){
+        if (arr[i] == true){
+        for (int j=i*i; j<=n; j=j+i){
+        arr[j] = false;
+            }
+        }}
+
+        for (int i=2; i<=n; i++){
+            if (arr[i] == true){
+             System.out.println(i);
+            }
+                
+    }
+        
+}
 }
