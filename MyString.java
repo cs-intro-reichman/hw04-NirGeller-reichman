87dc26a4c8849e7f7114bf2e1722529fc568cec3
NirGeller-reichman <nir.gellerchipchev@post.runi.ac.il>
2025-11-23 23:30:15 +0200

    first

--- MyString.java
+++ MyString.java
@@ -18,13 +18,43 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        for( int i = 0; i< str.length(); i++) {
+            char ch = str.charAt(i);
+            if( ch >= 65 && ch <= 90){
+               if (i==0)
+                str = (char)(ch + 32) + str.substring(1); 
+                else{
+                str = str.substring(0,i) + (char)(ch + 32) + str.substring(i+1);
+                }
+            }
+        
     }
+    return str;
+       }
+          
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        if (str2.length() > str1.length()){
         return false;
+        } 
+        else{
+            for(int i = 0; i <= str1.length() - str2.length(); i++){//unhappy    happy//i<=2
+                int index = i;//2
+                int j;
+                
+                for(j = 0; j < str2.length(); j++){//j<5
+                if (str1.charAt(index) != str2.charAt(j)){
+                 break;
+        }
+                    index++;//6//j4
+                }
+                
+                if (j ==str2.length()){
+                return true;
+                }
+            }
+            return false;
+        }
     }
 }
